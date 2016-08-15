7e333100a4f06a00d103880068f97a956b3caf5c
Paul Sinclair <bobsmith0030+git2@gmail.com>
2016-08-15 19:49:22 +0100

    Add first draft of analyser logic

--- analyser.py
+++ analyser.py
@@ -6,15 +6,66 @@ class Analyser:
     def __init__(self, x_size, y_size):
         self.x_size = x_size
         self.y_size = y_size
-        # all rectangular grids are definitely winning states (see move stealing argument)
-        self.winning_states = {Board(x,y) for x in range(1, x_size+1) for y in range(1, y_size+1)}
-        self.winning_states.difference_update({Board(1,1)})
+        # all rectangular grids other than 1x1 are definitely winning states (see move stealing argument)
+        self.winning_states = {Board(x, y) for x in range(1, x_size+1) for y in range(1, y_size+1)}
+        self.winning_states.difference_update({Board(1, 1)})
         self.losing_states = {Board(1, 1)}
-        self.to_check = {Board(1,1): False, **{state:True for state in self.winning_states}}
+        self.to_check_neighbours = {Board(1, 1)}
+        self.neighbours_checked = set()
 
+    def get_all_previous_states(self, board):
+        # TODO
+        return set()
 
-    def analyse(self):
-        try:
-            state_to_analyse, is_winning = self.to_check.popitem()
-        except KeyError:
-            return self.winning_states
+    def get_all_next_states(self, board):
+        # TODO
+        return set()
+
+    def analyse_game(self):
+        while True:
+            try:
+                state_to_analyse = self.to_check_neighbours.pop()
+                self.analyse_neighbours(state_to_analyse)
+            except KeyError:
+                return self.losing_states
+
+    def analyse_neighbours(self, state_to_analyse):
+        neighbours = self.get_all_previous_states(state_to_analyse)
+        for state in neighbours:
+            if state not in self.neighbours_checked:
+                self.to_check_neighbours.update(state)
+
+            if state not in self.winning_states and state not in self.losing_states:
+                # if we haven't calculated whether this state is a winning or losing state yet, do so now
+                self.evaluate(state)
+
+        self.neighbours_checked.update(state_to_analyse)
+
+    def evaluate(self, state):
+        # Returns True if this state is a winning state, or False if not
+        is_winner = self.quick_eval(state)
+        if is_winner is not None:
+            return is_winner
+
+        for next_state in self.get_all_next_states(state):
+            self.evaluate(next_state)
+            is_winner = self.quick_eval(state)
+            if is_winner is not None:
+                return is_winner
+
+        raise Exception("State evaluation failed despite all next states being evaluated")
+
+    def quick_eval(self, state):
+        # Returns True if this state is a winning state, False if not, None if unknown yet
+        # Also updates self.winning_states/self.losing_states appropriately
+
+        # if any next state is a losing state, this is a winning state
+        for next_state in self.get_all_next_states(state):
+            if next_state in self.losing_states:
+                self.winning_states.update(state)
+                return True
+        # if every next state is a winning state, this is a losing state
+        if self.get_all_next_states(state).issubset(self.winning_states):
+            self.losing_states.update(state)
+            return False
+        return None
